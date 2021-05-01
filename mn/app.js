8c23ab992a6d9b62dc105aafe005a4b1a931249c
Saira Awan <64429895+SairaAwan98@users.noreply.github.com>
2021-05-01 21:35:41 +0500

    Add files via upload

--- mn/app.js
+++ mn/app.js
@@ -3,8 +3,33 @@ function optionone(){
     var beta = parseInt(document.getElementById("Radio2").value);
     if(document.getElementById("Radio1").checked){
         document.getElementById('one').style.display = "block";
+        document.getElementById('two').style.display ="none";
     }
     if(document.getElementById("Radio2").checked){
         document.getElementById('two').style.display = "block";
+        document.getElementById('one').style.display = "none";
     }
 }
+function show(){
+
+    document.getElementById('solution').style.display = "block"; 
+}
+function solve(){
+    var table = document.getElementById('tab');
+    var row = document.getElementById('rows').value;
+    var col = document.getElementById('cols').value;
+
+    for(var i=0 ; i<row;i++){
+        var tr = document.createElement('tr');
+        for(var j=0; j<col;j++){
+            var td = document.createElement('td');
+            var text = document.createTextNode('canel '+j);
+
+            td.appendChild(text);
+            tr.appendChild(td);
+
+        }
+        table.appendChild(tr);
+    }  
+}
+document.getElementById('solve').addEventListener("click",solve);
